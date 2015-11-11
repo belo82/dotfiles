f06de2b89250babe57af6ccd930cc3a30886a34b
Peter Belko <peter.belko@condenastint.com>
2015-11-11 14:02:14 +0000

    Add ancient plugin to default lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,9 @@
 {:user {:plugins 	[[bn-service/lein-template "1.0.2"]
-                         [lein-deps-tree "0.1.2"]
-                         [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                         [lein-kibit "0.0.8"]
-                         [lein-pprint "1.1.1"]]
+                     [lein-ancient "0.6.7"]
+                     [lein-deps-tree "0.1.2"]
+                     [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                     [lein-kibit "0.0.8"]
+                     [lein-pprint "1.1.1"]]
 
         :dependencies [[slamhound "1.5.5"]
                        [org.clojure/tools.nrepl "0.2.11"]]
