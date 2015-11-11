cd23210ac8c204499b542fabece4b83fabbdef57
Peter Belko <peter.belko@condenastint.com>
2015-11-11 14:01:48 +0000

    Use relative line numbers in emacs

--- .emacs.d/evil.el
+++ .emacs.d/evil.el
@@ -1,6 +1,14 @@
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
+(unless (package-installed-p 'relative-line-numbers)
+  (package-install 'relative-line-numbers))
+
+; Display relative line numbers
+(add-hook 'prog-mode-hook 'relative-line-numbers-mode t)
+(add-hook 'prog-mode-hook 'line-number-mode t)
+(add-hook 'prog-mode-hook 'column-number-mode t)
+
 ; Give us back Ctrl+U for vim emulation
 (setq evil-want-C-u-scroll t)
 
