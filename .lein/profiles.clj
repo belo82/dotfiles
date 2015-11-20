4e8fc46516df0c81d520397117f785fec64dfaf6
Peter Belko <peter.belko@condenastint.com>
2015-11-20 16:07:45 +0000

    Move cider repl plugin to :repl profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins 	[[bn-service/lein-template "1.0.2"]
-                     [lein-ancient "0.6.7"]
-                     [lein-deps-tree "0.1.2"]
-                     [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                     [lein-kibit "0.0.8"]
-                     [lein-pprint "1.1.1"]]
+{:user {:plugins [[bn-service/lein-template "1.0.2"]
+                  [lein-ancient "0.6.7"]
+                  [lein-deps-tree "0.1.2"]
+                  [lein-kibit "0.0.8"]
+                  [lein-pprint "1.1.1"]]
 
         :dependencies [[slamhound "1.5.5"]
                        [org.clojure/tools.nrepl "0.2.11"]]
 
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
